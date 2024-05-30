be8f96120c657fdae7391639e2842f367acb86b4
juleshkumar <154207267+juleshkumar@users.noreply.github.com>
2024-05-30 13:02:02 +0530

    Update backend.tf

--- julesh-terraform/environments/dev/load_balancer/backend.tf
+++ julesh-terraform/environments/dev/load_balancer/backend.tf
@@ -3,5 +3,6 @@ terraform {
     bucket     = "terrafrom-test-to-delete-bucket"
     key        = "backend/load"
     region     = "ap-south-1"
+    role_arn   = "arn:aws:iam::471112548391:role/decimal-jenkins-role"
   }
 }
