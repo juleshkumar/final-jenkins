11c6839710579918a6300dade12676c0c04b70d4
juleshkumar <154207267+juleshkumar@users.noreply.github.com>
2024-05-30 13:01:19 +0530

    Update backend.tf

--- julesh-terraform/environments/dev/elasticache/backend.tf
+++ julesh-terraform/environments/dev/elasticache/backend.tf
@@ -3,6 +3,7 @@ terraform {
     bucket     = "terrafrom-test-to-delete-bucket"
     key        = "backend/ec"
     region     = "ap-south-1"
+    role_arn   = "arn:aws:iam::471112548391:role/decimal-jenkins-role"
   }
 }
 
