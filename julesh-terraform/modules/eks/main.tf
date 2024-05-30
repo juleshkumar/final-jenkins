1f88c8d1d6d35fd6bab4168028e4296ca492e269
juleshkumar <154207267+juleshkumar@users.noreply.github.com>
2024-05-30 12:36:58 +0530

    Update main.tf

--- julesh-terraform/modules/eks/main.tf
+++ julesh-terraform/modules/eks/main.tf
@@ -5,6 +5,7 @@ data "terraform_remote_state" "vpc_state" {
     bucket     = "terrafrom-test-to-delete-bucket"
     key        = "backend/vpc"
     region     = "ap-south-1"
+    role_arn   = ""
   }
 }
 
@@ -15,6 +16,7 @@ data "terraform_remote_state" "kms" {
     bucket     = "terrafrom-test-to-delete-bucket"
     key        = "backend/kms"
     region     = "ap-south-1"
+    role_arn   = ""
   }
 }
 
