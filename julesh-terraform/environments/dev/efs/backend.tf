e5f8e4a0627cacfddcaac19538d68a8abb82154a
juleshkumar <154207267+juleshkumar@users.noreply.github.com>
2024-05-30 13:21:33 +0530

    Update backend.tf

--- julesh-terraform/environments/dev/efs/backend.tf
+++ julesh-terraform/environments/dev/efs/backend.tf
@@ -3,7 +3,7 @@ terraform {
     bucket     = "terrafrom-test-to-delete-bucket"
     key        = "backend/efs"
     region     = "ap-south-1"
-    role_arn   = "arn:aws:iam::471112548391:role/decimal-jenkins-role"
+    role_arn   = "arn:aws:iam::471112548391:role/decimal-vrt-test"
   }
 }
 
